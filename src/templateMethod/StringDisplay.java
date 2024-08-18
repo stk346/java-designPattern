a6b761de0bd517d12e803c57ba4015addc744f4c
stk346 <stk346@gamil.com>
2024-08-19 00:50:10 +0900

    templateMethod

---
+++ src/templateMethod/StringDisplay.java
@@ -0,0 +1,35 @@
+package templateMethod;
+
+public class StringDisplay extends AbstractDisplay {
+
+    private String string; // 표시해야 하는 문자
+    private int width;
+
+    public StringDisplay(String string) {
+        this.string = string;
+        this.width = string.length();
+    }
+
+    @Override
+    public void open() {
+        printLine();
+    }
+
+    @Override
+    public void print() {
+        System.out.println("|" + string + "|");
+    }
+
+    @Override
+    public void close() {
+        printLine();
+    }
+
+    private void printLine() {
+        System.out.print("+");
+        for (int i = 0; i < width; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+}
